62e24974c0b08d6d7a7fd31937679c377a4e9b10
hyunkang07 <hyunkang007@gmail.com>
2025-05-13 11:41:10 +0900

    app.js
    app.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,11 @@
-
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App(){
   return(
-    <></>
+    <BrowserRouter>
+        <Navbar />
+    </BrowserRouter>
   )
 }
 
--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -4,11 +4,6 @@ function Navbar(){
     return(
         <nav>
             <ul>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">ABOUT</a></li>
-                <li><a href="#">CONTACT</a></li>
-            </ul>
-            <ul>
                 <li><Link to="/">HOME</Link></li>
                 <li><Link to="/About">ABOUT</Link></li>
                 <li><Link to="/Contact">CONTACT</Link></li>
