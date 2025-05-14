26752090792547e88f7f6ed073d1f1c980e415f7
hyunkang07 <hyunkang007@gmail.com>
2025-05-14 10:02:19 +0900

    ts
    ts

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,19 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes , Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import './styles/global.css'
 
 function App(){
   return(
     <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/About" element={<About/>} />
+          <Route path="/Contact" element={<Contact/>} />
+        </Routes>
     </BrowserRouter>
   )
 }
---
+++ app/src/pages/Home.js
@@ -0,0 +1,11 @@
+import '../styles/subpage.css'
+
+function Home(){
+    return(
+        <div>
+            <h2>HOME</h2>
+        </div>
+    )
+}
+
+export default Home;
