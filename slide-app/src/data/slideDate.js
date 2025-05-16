f42e5803d4fd084aef831274598d199be094b54e
hyunkang07 <hyunkang007@gmail.com>
2025-05-16 15:12:55 +0900

    이미지 경로값
    이미지 경로값

--- slide-app/src/data/slideDate.js
+++ slide-app/src/data/slideDate.js
@@ -2,9 +2,9 @@ const slideDate= [
     {
         title:'인기상품',
         items:[
-            {title:'현수 무료나눔' , image: './images/a.jfif'},
-            {title:'수아 ?원' , image: './images/b.jfif'},
-            {title:'영원 0원' , image: './images/c.jfif'},
+            {title:'현수 무료나눔' , image: '../assets/images/a.png'},
+            {title:'수아 ?원' , image: '../assets/images/b.png'},
+            {title:'영원 0원' , image: '../assets/images/c.png'},
         ]
     }
 ]
